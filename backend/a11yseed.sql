8c9dcfe3ff999768f871e136eff155b44759f1fa
Lucia Cerchie <luciacerchie@gmail.com>
2021-01-29 10:06:16 -0700

    adds seed file

---
+++ backend/a11yseed.sql
@@ -0,0 +1,8 @@
+-- test user password is "alligator"
+INSERT INTO users(username, password)
+VALUES ('testuserKate',
+'$2a$10$ktsFhrylH3ngy5j.hrEopu1qYI67WzqjH4CYHEcfdOorRTx9i48Ta'),
+('testuserJane',
+'$2a$10$ktsFhrylH3ngy5j.hrEopu1qYI67WzqjH4CYHEcfdOorRTx9i48Ta'),
+('testuserJack',
+'$2a$10$ktsFhrylH3ngy5j.hrEopu1qYI67WzqjH4CYHEcfdOorRTx9i48Ta');
