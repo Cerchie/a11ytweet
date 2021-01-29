c547f8be00309529d5127cafcec528681f73a6ba
Lucia Cerchie <luciacerchie@gmail.com>
2021-01-29 09:05:52 -0700

    adds db using pg

---
+++ backend/db.js
@@ -0,0 +1,15 @@
+
+"use strict";
+
+/** Database setup for jobly. */
+
+const { Client } = require("pg");
+const { getDatabaseUri } = require("./config");
+
+const db = new Client({
+  connectionString: getDatabaseUri(),
+});
+
+db.connect();
+
+module.exports = db;
