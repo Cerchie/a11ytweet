68476bbb6799bc6c5ec0b89ef2e66bd2c15e6699
Lucia Cerchie <luciacerchie@gmail.com>
2021-01-29 09:10:40 -0700

    adds config with colors, test db, bcrypt, fake secret key for now

---
+++ backend/config.js
@@ -0,0 +1,38 @@
+"use strict";
+
+/** Shared config for application; can be required many places. */
+
+require("dotenv").config();
+require("colors"); //package that prints out colors to console to make msgs easier to read
+
+const SECRET_KEY = process.env.SECRET_KEY || "secret-dev";
+
+const PORT = +process.env.PORT || 3001;
+
+// Use dev database, testing database, or via env var, production database
+function getDatabaseUri() {
+  return (process.env.NODE_ENV === "test")
+      ? "a11y_test"
+      : process.env.DATABASE_URL || "a11y";
+}
+
+// Speed up bcrypt during tests, since the algorithm safety isn't being tested
+//
+// WJB: Evaluate in 2021 if this should be increased to 13 for non-test use
+const BCRYPT_WORK_FACTOR = process.env.NODE_ENV === "test" ? 1 : 12;
+
+console.log("a11y Config:".green);
+console.log("SECRET_KEY:".yellow, SECRET_KEY);
+console.log("PORT:".yellow, PORT.toString());
+console.log("BCRYPT_WORK_FACTOR".yellow, BCRYPT_WORK_FACTOR);
+console.log("Database:".yellow, getDatabaseUri());
+console.log("---");
+
+module.exports = {
+  SECRET_KEY,
+  PORT,
+  BCRYPT_WORK_FACTOR,
+  getDatabaseUri,
+};
+
+//config pattern copied from bootcamp project
