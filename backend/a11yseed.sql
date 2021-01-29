1375e27420b05a6823118246cb5d63e18be3cabf
Lucia Cerchie <luciacerchie@gmail.com>
2021-01-29 10:20:05 -0700

    creates seed file

--- backend/a11yseed.sql
+++ backend/a11yseed.sql
@@ -1,4 +1,17 @@
--- test user password is "alligator"
+-- seed user password to experiment with is "alligator"
+
+
+DROP DATABASE a11y;
+
+CREATE DATABASE a11y;
+\connect a11y;
+
+CREATE TABLE users(
+    username TEXT UNIQUE NOT NULL,
+    password TEXT NOT NULL
+
+);
+
 INSERT INTO users(username, password)
 VALUES ('testuserKate',
 '$2a$10$ktsFhrylH3ngy5j.hrEopu1qYI67WzqjH4CYHEcfdOorRTx9i48Ta'),
@@ -6,3 +19,6 @@ VALUES ('testuserKate',
 '$2a$10$ktsFhrylH3ngy5j.hrEopu1qYI67WzqjH4CYHEcfdOorRTx9i48Ta'),
 ('testuserJack',
 '$2a$10$ktsFhrylH3ngy5j.hrEopu1qYI67WzqjH4CYHEcfdOorRTx9i48Ta');
+
+
+-- command to run from cli: psql -d a11y -a -f a11yseed.sql
