217279c5af433253c64693a64aedc9d755fd1e2d
Lucia Cerchie <luciacerchie@gmail.com>
2021-02-02 14:58:21 -0700

    adds users routes and begins process of adapting user tests from project

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -8,7 +8,7 @@ const User = require('../models/user')
 const { createToken } = require('../helpers/tokens')
 const userNewSchema = require('../schemas/userNew.json')
 const userUpdateSchema = require('../schemas/userUpdate.json')
-
+//TODO-- add schemas
 const router = express.Router()
 
 router.post('/', async function (req, res, next) {
@@ -26,4 +26,40 @@ router.post('/', async function (req, res, next) {
         return next(err)
     }
 })
+//get route
+router.get('/:username', ensureCorrectUser, async function (req, res, next) {
+    try {
+        const user = await User.get(req.params.username)
+        return res.json({ user })
+    } catch (err) {
+        return next(err)
+    }
+})
+//patch route for updates
+router.patch('/:username', ensureCorrectUser, async function (req, res, next) {
+    try {
+        const validation = validate(req.body, userUpdateSchema)
+        if (!validation.valid) {
+            throw new ExpressError(
+                validation.errors.map((e) => e.stack),
+                400
+            )
+        }
+        const user = await User.update(req.params.username, req.body)
+        return res.json({ user })
+    } catch (err) {
+        return next(err)
+    }
+})
+//delete route
+router.delete('/:username', ensureCorrectUser, async function (req, res, next) {
+    try {
+        await User.remove(req.params.username)
+        return res.json({ message: 'User deleted' })
+    } catch (err) {
+        return next(err)
+    }
+})
+
 module.exports = router
+//code partially adapted from Springboard solution https://github.com/Cerchie/react-jobly/blob/main/backend/routes/users.js
