dbba2822efcb966f8e4a6ce0af9598c3ac6b0f65
Lucia Cerchie <luciacerchie@gmail.com>
2021-02-09 11:07:02 -0700

    adds form logic to login User, api file

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,11 +3,37 @@ import LinkList from './LinkList'
 import Login from './Login'
 import NavBar from './NavBar'
 import Signup from './Signup'
+import Api from './Api'
 import React from 'react'
 import { Route, Router, Switch, BrowserRouter } from 'react-router-dom'
 
 //renders not-logged in version of app
 function App() {
+    //function for passing in to allow user to signup
+    async function signupUser(signupData) {
+        try {
+            let token = await Api.signup(signupData)
+            setToken(token)
+            let currentUser = await Api.getCurrentUser(signupData.username)
+            setCurrentUser(currentUser)
+            return { success: true }
+        } catch (errors) {
+            console.error('signup failed', errors)
+            return { success: false, errors }
+        }
+    }
+    //passing in to user to allow login
+    async function loginUser(signupData) {
+        try {
+            let token = await Api.login(signupData)
+            setToken(token)
+            return { success: true }
+        } catch (errors) {
+            console.error('login failed', errors)
+            return { success: false, errors }
+        }
+    }
+
     return (
         <div className="App">
             <header className="App-header">
@@ -22,7 +48,7 @@ function App() {
                             <LinkList />
                         </Route>
                         <Route exact path="/login">
-                            <Login />
+                            <Login login={loginUser} />
                         </Route>
                         <Route exact path="/signup">
                             <Signup />
--- frontend/src/Login.js
+++ frontend/src/Login.js
@@ -1,7 +1,77 @@
-import React from 'react'
+import { React, useState } from 'react'
 
-function Login() {
-    return <div>Login comp here</div>
+const Login = ({ login }) => {
+    //setting initial state var
+    const INITIAL_STATE = {
+        username: '',
+        password: '',
+    }
+    //form data and errs in state
+    const [formData, setFormData] = useState(INITIAL_STATE)
+    const [formErrors, setFormErrors] = useState([])
+    //handle form changes
+    const handleChange = (e) => {
+        const { name, value } = e.target
+        setFormData((formData) => ({
+            ...formData,
+            [name]: value,
+        }))
+    }
+    //handles form submit
+    async function handleSubmit(e) {
+        e.preventDefault()
+        let result = await login(formData)
+        console.log(result)
+        if (result.success) {
+            history.push('/companies')
+        } else {
+            setFormErrors(result.errors)
+        }
+        setFormData(INITIAL_STATE)
+    }
+
+    //making debugging easier
+    console.debug(
+        'Login',
+        'login=',
+        typeof login,
+        'formData=',
+        formData,
+        'formErrors=',
+        formErrors
+    )
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <div className="form-group">
+                <label>Username</label>
+                <input
+                    name="username"
+                    className="form-control"
+                    value={formData.username}
+                    onChange={handleChange}
+                />
+            </div>
+            <div className="form-group">
+                <label>Password</label>
+                <input
+                    type="password"
+                    name="password"
+                    className="form-control"
+                    value={formData.password}
+                    onChange={handleChange}
+                />
+            </div>
+
+            <button
+                type="submit"
+                className="btn btn-primary float-right"
+                onSubmit={handleSubmit}
+            >
+                Submit
+            </button>
+        </form>
+    )
 }
-
+//code adapted from my project https://github.com/Cerchie/react-jobly/blob/main/frontend/src/Login.js
 export default Login
