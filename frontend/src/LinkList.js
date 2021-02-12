761c66ccb8a79b38e6d18ac1f224919bc3b227c7
Lucia Cerchie <luciacerchie@gmail.com>
2021-02-12 10:00:37 -0700

    modifies linklist, removes links file

--- frontend/src/LinkList.js
+++ frontend/src/LinkList.js
@@ -1,26 +1,24 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { useSelector } from 'react-redux'
-import CartIcons from './CartIcons'
 
 function LinkList() {
-    const products = useSelector((st) => st.products)
-    const productCards = Object.keys(products).map((id) => (
+    const links = useSelector((st) => st.list_items)
+    const linksListed = Object.keys(links).map((id) => (
         <div className="col-md-3 mb-3" key={id}>
             <div className="card">
                 <div className="card-body">
                     <h2 className="card-title text-center">
-                        <Link to={`/products/${id}`}>{products[id].name}</Link>
+                        <Link to={`/products/${id}`}>{links}</Link>
                     </h2>
-                    <CartIcons id={id} />
                 </div>
             </div>
         </div>
     ))
     return (
         <div>
-            <h4>Look at all of our beautiful products!</h4>
-            <div className="row">{productCards}</div>
+            <h4>Here are the links</h4>
+            <div className="row">{linksListed}</div>
         </div>
     )
 }
--- frontend/src/reducers/links.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-function links() {
-    return <div>Links here</div>
-}
-
-export default links
