37ebeb4176aa772bcdaba0824cf2e1316acef7da
Lucia Cerchie <luciacerchie@gmail.com>
2021-02-22 08:28:06 -0700

    adds fragment

--- frontend/src/GoHome.js
+++ frontend/src/GoHome.js
@@ -2,10 +2,10 @@ import React from 'react'
 
 function GoHome() {
     return (
-        <div>
+        <>
             Success! You've edited your account.
             <a href="/">Return home. </a>
-        </div>
+        </>
     )
 }
 
