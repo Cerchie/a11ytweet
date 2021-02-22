e08cddf58d9ba92264abb6ffc80a5a95013eaa8e
Lucia Cerchie <luciacerchie@gmail.com>
2021-02-22 09:43:49 -0700

    adding more styles

--- frontend/src/LinkList.js
+++ frontend/src/LinkList.js
@@ -2,13 +2,14 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import { useSelector, useDispatch } from 'react-redux'
 import LinksFromAPICall from './LinksFromAPICall'
+import './styles/LinkList.css'
 
 function LinkList() {
     const links = LinksFromAPICall()
 
     return (
         <>
-            <h4>Here are the links</h4>
+            <h2>a11y repos</h2>
             <p className="row">{links}</p>
         </>
     )
