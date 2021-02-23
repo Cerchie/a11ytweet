b723caea56fa8fd64dabb482bf7c42775ee0a809
Lucia Cerchie <luciacerchie@gmail.com>
2021-02-23 08:44:28 -0700

    adds import

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -10,6 +10,7 @@ import { persistStore, persistReducer } from 'redux-persist'
 import storage from 'redux-persist/lib/storage'
 import autoMergeLevel2 from 'redux-persist/lib/stateReconciler/autoMergeLevel2'
 import { PersistGate } from 'redux-persist/lib/integration/react'
+import './index.css'
 
 const persistConfig = {
     key: 'authType',
